d5150abbf21f9cf8b2166d8d1352003cd097d269
Hiroki Aika <hirohiroaika@outlook.jp>
2020-03-16 12:53:24 +0900

    firstテキスト表示

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 $(function () {
   $('.main').hide();
-  $('.opening p').hide();
+  $('.opening p').css({opacity: '0'});
+  setTimeout(function () {
+    $('.first').stop().animate({opacity: '1'}, 1000);
+  }, 3000);
 });
