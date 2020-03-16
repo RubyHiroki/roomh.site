6933becfdf922b5466bc275b79da55320bf0c79e
Hiroki Aika <hirohiroaika@outlook.jp>
2020-03-16 12:44:33 +0900

    オープニング画面を最大表示

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-$(function() {
-  $('.main').css({display: 'none'});
-  setTimeout(function() {
-  $('.main').stop().animate({display: 'block'}, 1000);
-}, 3000);
+$(function () {
+  $('.main').hide();
 });
